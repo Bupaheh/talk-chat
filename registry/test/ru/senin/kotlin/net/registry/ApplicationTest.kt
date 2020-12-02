e0d9a5dbf186c92f0d1808a78cd84332f55f1fe5
Pavel Balay <pavel.balai@gmail.com>
2020-12-02 22:19:37 +0300

    Change registration tests

--- registry/test/ru/senin/kotlin/net/registry/ApplicationTest.kt
+++ registry/test/ru/senin/kotlin/net/registry/ApplicationTest.kt
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import ru.senin.kotlin.net.UserAddress
 import ru.senin.kotlin.net.UserInfo
-import kotlin.reflect.typeOf
 import kotlin.test.*
 
 fun Application.testModule() {
@@ -43,47 +42,31 @@ class ApplicationTest {
     }
 
     @Test
-    fun `register user`() {
-        withTestApplication({ testModule() }) {
-            //registration test
-            handleRequest {
-                method = HttpMethod.Post
-                uri = "/v1/users"
-                addHeader("Content-type", "application/json")
-                setBody(objectMapper.writeValueAsString(userData))
-            }.apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-                val content = response.content ?: fail("No response content")
-                val info = objectMapper.readValue<HashMap<String, String>>(content)
+    fun `register user`() = withRegisteredTestUser {
 
-                assertNotNull(info["status"])
-                assertEquals("ok", info["status"])
-            }
+        //test with incorrect name
+        val testUserData = UserInfo("Test test", testHttpAddress)
+        handleRequest {
+            method = HttpMethod.Post
+            uri = "/v1/users"
+            addHeader("Content-type", "application/json")
+            setBody(objectMapper.writeValueAsString(testUserData))
+        }.apply {
+            assertEquals(HttpStatusCode.BadRequest, response.status())
+            val content = response.content ?: fail("No response content")
+            assertEquals("Illegal user name", content)
+        }
 
-            //test with incorrect name
-            val testUserData = UserInfo("Test test", testHttpAddress)
-            handleRequest {
-                method = HttpMethod.Post
-                uri = "/v1/users"
-                addHeader("Content-type", "application/json")
-                setBody(objectMapper.writeValueAsString(testUserData))
-            }.apply {
-                assertEquals(HttpStatusCode.BadRequest, response.status())
-                val content = response.content ?: fail("No response content")
-                assertEquals("Illegal user name", content)
-            }
-
-            //user already registered test
-            handleRequest {
-                method = HttpMethod.Post
-                uri = "/v1/users"
-                addHeader("Content-type", "application/json")
-                setBody(objectMapper.writeValueAsString(userData))
-            }.apply {
-                assertEquals(HttpStatusCode.Conflict, response.status())
-                val content = response.content ?: fail("No response content")
-                assertEquals("User already registered", content)
-            }
+        //user already registered test
+        handleRequest {
+            method = HttpMethod.Post
+            uri = "/v1/users"
+            addHeader("Content-type", "application/json")
+            setBody(objectMapper.writeValueAsString(userData))
+        }.apply {
+            assertEquals(HttpStatusCode.Conflict, response.status())
+            val content = response.content ?: fail("No response content")
+            assertEquals("User already registered", content)
         }
     }
 
