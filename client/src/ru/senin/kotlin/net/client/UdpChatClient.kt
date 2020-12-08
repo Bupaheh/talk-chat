76e1da26860653586351ef605be60382b5c624d5
receed <efremov-343@yandex.ru>
2020-12-08 18:54:16 +0300

    Add UDP client

--- client/src/ru/senin/kotlin/net/client/UdpChatClient.kt
+++ client/src/ru/senin/kotlin/net/client/UdpChatClient.kt
@@ -1,9 +1,25 @@
 package ru.senin.kotlin.net.client
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.core.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import ru.senin.kotlin.net.Message
+import java.net.InetSocketAddress
 
 class UdpChatClient(host: String, port: Int) : ChatClient {
-    override fun sendMessage(message: Message) {
+    private val address = InetSocketAddress(host, port)
+    private val socket = aSocket(ActorSelectorManager(Dispatchers.IO)).udp().connect(address)
+    private val objectMapper = jacksonObjectMapper()
 
+    override fun sendMessage(message: Message) {
+        val text = objectMapper.writeValueAsString(message)
+        val packetBuilder = BytePacketBuilder()
+        packetBuilder.writeText(text)
+        runBlocking {
+            socket.send(Datagram(packetBuilder.build(), address))
+        }
     }
 }
--- client/src/ru/senin/kotlin/net/server/UdpChatServer.kt
+++ client/src/ru/senin/kotlin/net/server/UdpChatServer.kt
@@ -1,25 +1,19 @@
 package ru.senin.kotlin.net.server
 
 import com.fasterxml.jackson.module.kotlin.readValue
-import io.ktor.application.*
-import io.ktor.http.cio.websocket.*
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
-import io.ktor.util.cio.*
-import io.ktor.utils.io.*
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import ru.senin.kotlin.net.Message
 import java.net.InetSocketAddress
 
 class UdpChatServer(host: String, port: Int) : ChatServer(host, port) {
-    private val server = aSocket(ActorSelectorManager(Dispatchers.IO)).udp().bind(InetSocketAddress("127.0.0.1", 2323))
+    private val socket = aSocket(ActorSelectorManager(Dispatchers.IO)).udp().bind(InetSocketAddress(host, port))
 
     override fun start() {
         runBlocking {
-            for (datagram in server.incoming) {
+            for (datagram in socket.incoming) {
                 val text = datagram.packet.readText()
                 val message = objectMapper.readValue<Message>(text)
                 listener?.messageReceived(message.user, message.text)
